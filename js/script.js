df9db12652c105d9112d053b91607d814165ef24
ttooaa2020 <sashin9622@gmail.com>
2025-01-14 17:50:45 +0900

    페이지 진행중

--- js/script.js
+++ js/script.js
@@ -94,6 +94,7 @@ $(function () {
 
     autoplay: {
       delay: 0,
+ 
     },
   });
 
@@ -114,27 +115,30 @@ $(function () {
   const $question = $(".question-list > li");
   const $answer = $(".answer-wrap");
   const $questionList = $(".question-list");
+  const duration = 300; // 애니메이션 지속 시간 추가
 
   // 초기 상태 설정
   $answer.hide();
 
   // 질문을 클릭했을 때
-  $question.on("click", function (e) {
-    e.stopPropagation(); // 이벤트 버블링 방지
-    // 선택한 항목을 제외한 다른 항목들의 on 클래스 제거 및 답변 숨기기
-    $(this).siblings().removeClass("on").find($answer).stop().slideUp(duration);
+  $question.on("click", function () {
+    const $this = $(this);
+    const $thisAnswer = $this.find($answer);
 
-    // 선택한 항목의 on 클래스 토글 및 답변 토글
-    $(this).toggleClass("on");
-    $(this).find($answer).stop().slideToggle(duration);
+    // 다른 답변들 닫기
+    $question.not($this).removeClass("on");
+    $answer.not($thisAnswer).slideUp(duration);
+
+    // 현재 답변 토글
+    $this.toggleClass("on");
+    $thisAnswer.slideToggle(duration);
   });
 
-  // 문서 전체에 클릭 이벤트 추가
+  // 문서 클릭 이벤트는 필요한 경우에만 사용
   $(document).on("click", function (e) {
-    // 클릭된 요소가 질문 리스트 내부가 아닐 경우
     if (!$(e.target).closest($questionList).length) {
       $question.removeClass("on");
-      $answer.stop().slideUp(duration);
+      $answer.slideUp(duration);
     }
   });
 
